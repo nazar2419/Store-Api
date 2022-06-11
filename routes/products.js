7df7056c608e1eff27916007c021726c2f1dcee9
nazar2419 <nazarx095@gmail.com>
2022-06-11 14:12:23 +0300

    conect Populate DB

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+
 const {getAllProducts, getAllProductsStatic} = require('../controllers/products');
 
 router.route('/').get(getAllProducts);
